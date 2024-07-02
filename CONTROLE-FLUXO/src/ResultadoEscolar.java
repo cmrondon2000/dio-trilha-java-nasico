2b9aea115b986f449f9d03750d1573fd9a0feb21
cmrondon2000 <cm.rondon2000@gmail.com>
2024-07-02 11:48:47 -0300

    Condicionantes

---
+++ CONTROLE-FLUXO/src/ResultadoEscolar.java
@@ -0,0 +1,19 @@
+public class ResultadoEscolar {
+
+    public static void main(String[] args) {
+        
+        int nota = 6;
+       
+       if(nota >= 7)
+        System.out.println("Aprovado");
+
+        else if (nota < 7 && nota >=5 ) 
+
+            System.out.println("Prova Recuperacao");
+                  
+        
+       else
+        System.out.println("Reprovado");
+    }
+    
+}
--- Tipos-Variaveis/src/TiposVariaveis.java
+++ Tipos-Variaveis/src/TiposVariaveis.java
@@ -1,6 +1,8 @@
 public class TiposVariaveis {
     public static void main(String[] args)  {
         String nomecompleto = "Linguaem" + "JAVA";
+
+        
         System.out.println(nomecompleto);
     }
 }
