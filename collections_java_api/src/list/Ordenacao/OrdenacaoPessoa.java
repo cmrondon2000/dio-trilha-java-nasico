c6acf24c0622b27de8da943898051ed35adaf350
cmrondon2000 <cm.rondon2000@gmail.com>
2024-07-31 22:49:33 -0300

    segundapartecomparator

--- collections_java_api/src/list/Ordenacao/OrdenacaoPessoa.java
+++ collections_java_api/src/list/Ordenacao/OrdenacaoPessoa.java
@@ -19,7 +19,7 @@ public class OrdenacaoPessoa {
 
     }
 
-    public List<Pessoa> ordenadoPorIdade(){
+    public List<Pessoa> ordenarPorIdade(){
 
         List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
         Collections.sort(pessoasPorIdade);
@@ -36,5 +36,26 @@ public class OrdenacaoPessoa {
           throw new RuntimeException("A lista está vazia!");
         }
       }
+
+      public static void main(String[] args) {
+        // Criando uma instância da classe OrdenacaoPessoas
+          OrdenacaoPessoa ordenacaoPessoas = new OrdenacaoPessoa();
+      
+          // Adicionando pessoas à lista
+          ordenacaoPessoas.adicionarPessoa("Alice", 20, 1.56);
+          ordenacaoPessoas.adicionarPessoa("Bob", 30, 1.80);
+          ordenacaoPessoas.adicionarPessoa("Charlie", 25, 1.70);
+          ordenacaoPessoas.adicionarPessoa("David", 17, 1.56);
+      
+          // Exibindo a lista de pessoas adicionadas
+          System.out.println(ordenacaoPessoas.pessoaList);
+      
+          // Ordenando e exibindo por idade
+          System.out.println(ordenacaoPessoas.ordenarPorIdade());
+      
+          // Ordenando e exibindo por altura
+          System.out.println(ordenacaoPessoas.ordenarPorAltura());
+        }
+
 
 }
